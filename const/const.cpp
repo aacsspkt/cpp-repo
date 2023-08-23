06448f8fb096b2fd3117b2f8e017d0105b152925
Ashish Sapkota <aacsspkt3@gmail.com>
2023-08-23 18:05:37 +0545

    constants

---
+++ const/const.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    /*
+    constant can be created using 'const' keyword in left side of variable declared.
+    Using 'const' will declared variable readonly and compile with check that it is
+    unchanged. It helps in data security.
+    */
+
+    const double PI = 3.14159;
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+
+
+    return 0;
+}
