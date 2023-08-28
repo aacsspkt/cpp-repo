baa78d92067e837f5a4679873336ab1c8c5a7317
Ashish Sapkota <aacsspkt3@gmail.com>
2023-08-28 23:22:23 +0545

    multidimensional array

---
+++ 35multidimensionarray/MultiDimensionalArray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+    std::string cars[][3] = {
+        {"Mustang", "Escape", "F-150"},
+        {"Corvette", "Equinox", "Silverado"},
+        {"Challenger", "Durango", "Ram 1500"}};
+
+    std::cout << cars[0][0] << " ";
+    std::cout << cars[0][1] << " ";
+    std::cout << cars[0][2] << " ";
+    std::cout << cars[1][0] << " ";
+    std::cout << cars[1][1] << " ";
+    std::cout << cars[1][2] << " ";
+    std::cout << cars[2][0] << " ";
+    std::cout << cars[2][1] << " ";
+    std::cout << cars[2][2] << " ";
+
+    std::cout << "\n";
+
+    int rows = sizeof(cars)/sizeof(cars[0]);
+    int colums = sizeof(cars[0])/sizeof(cars[0][0]);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < colums; j++) {
+            std::cout << cars[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
