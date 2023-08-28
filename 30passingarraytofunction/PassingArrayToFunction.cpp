a79264d26a89b3721233089c1b10e6b89ce83cf6
Ashish Sapkota <aacsspkt3@gmail.com>
2023-08-28 19:30:44 +0545

    passing array to function

--- 30passingarraytofunction/PassingArrayToFunction.cpp
+++ 30passingarraytofunction/PassingArrayToFunction.cpp
@@ -23,7 +23,9 @@ as parameter into the function
 double getTotal(double prices[], int size) {
     double total = 0;
     
-    for(int i = 0; i< ; i++) {
-
+    for(int i = 0; i< size; i++) {
+        total+= prices[i];
     }
+    
+    return total;
 }
