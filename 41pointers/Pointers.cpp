b9b93439d1f30c7748049b7dd9bb5d66c3a61882
Ashish Sapkota <aacsspkt3@gmail.com>
2023-08-29 22:14:03 +0545

    null pointers

--- 41pointers/Pointers.cpp
+++ 41pointers/Pointers.cpp
@@ -25,7 +25,31 @@ int main()
     std::cout << "Value of age: " << *pAge << "\n";
 
     std::cout << "Address of freePizzas: " << pFreePizzas << "\n";
-    std::cout << "First value of freePizzas: " << *pFreePizzas;
+    std::cout << "First value of freePizzas: " << *pFreePizzas << "\n";
 
+
+    /*
+    Null value is a special value that means something has no value.
+    When a pointer is holding a null value, that pointer is not pointing
+    at anything (null pointer)
+
+    nullptr = keyword represents a null pointer literal
+
+    nullptrs are helpful when determining if an address was
+    successfully assigned to a pointer.
+    */
+
+    int *pointer = nullptr;
+    int x = 123;
+
+    // uncomment below to assigne address to pointer
+    // pointer = &x;
+
+    if(pointer == nullptr) {
+        std::cout << "Address was not assigned\n";
+    } else {
+        std::cout << "Address was assigned\n";
+        std::cout << "Value at that address is: " << *pointer;
+    }
     return 0;
 }
